44cfac9d977a1662e43e7ce30634bb75d7e54d6e
damaleparas <damaleparas@gmail.com>
2025-09-05 09:54:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # human redable    
 @app.get("/")
 def home():
-    return {"message":"Insurence premium prediction api"}
+    return {"message":"Insurence premium prediction api for prediction runt the streamlit file in gitub on local pc using [streamlit run frontend.py] . the endpoint are '/predict'" }
 # machine redable (like kubernet)
 @app.get("/health")
 def health_check():
